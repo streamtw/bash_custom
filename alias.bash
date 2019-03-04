419cbdd00ec82cd2fb2e8e4d60992ca5b9aa37d0
Stream <stream@unisharp.com>
2019-03-04 11:42:58 +0800

    add alias and ps1

---
+++ alias.bash
@@ -0,0 +1,18 @@
+alias ls='ls --color=auto'
+alias 0='php artisan serve --host=0.0.0.0:8000 &> /dev/null &'
+alias ip="ifconfig | grep 192 | sed 's/.*inet //' | sed 's/ netmask.*//'"
+alias es='elasticsearch &> /dev/null &'
+alias php7='brew unlink php55 && brew link php71 && brew services stop php55 && brew services start php71'
+alias php5='brew unlink php71 && brew link php55 && brew services stop php71 && brew services start php55'
+alias killphp="kill \$(ps aux | grep [p]hp | awk '{print \$2}')"
+alias weather='curl -4 http://wttr.in/taipei'
+alias battery='pmset -g batt'
+alias t='vendor/bin/phpunit'
+alias lfm='cd ~/projects/lfm-dev/vendor/unisharp/laravel-filemanager'
+alias y='HOST=0.0.0.0 API_BASE=http://'$(ip)':8000/api/v1 yarn dev'
+alias qa='git checkout qa && git pull && git merge develop && git push && git checkout develop'
+alias master='git checkout master && git pull && git merge qa && git push && git checkout develop'
+alias prod='git checkout prod && git pull && git merge master && git push && git checkout develop'
+alias sass='npm rebuild node-sass --force'
+alias mysql_container='docker run -d --name mysql -p 3306:3306 -v /data/mysql:/var/lib/mysql -e 
+MARIADB_ALLOW_EMPTY_PASSWORD=true mariadb/server:10.3'
---
+++ ps1.bash
@@ -0,0 +1,16 @@
+function parse_git_branch {
+    git branch 2> /dev/null | grep '*' | sed 's/*/ 💠/'
+}
+
+export GRAY='\[\033[1;90m\]'
+export GRAY_ALT='\[\033[0;30m\]'
+export RED='\[\033[0;31m\]'
+export GREEN='\[\033[0;32m\]'
+export YELLOW='\[\033[0;33m\]'
+export BLUE='\[\033[0;34m\]'
+export MAGENTA='\[\033[0;35m\]'
+export CYAN='\[\033[0;36m\]'
+export NC='\[\033[0m\]'
+export CLICOLOR=1
+export LSCOLORS=Exfxcxdxbxegedabagacad
+export PS1=$YELLOW'👑 \u'$GRAY' 📂 \W'$CYAN'$(parse_git_branch)\n'$GREEN'🌏 \t'$RED' ➜ '$NC
