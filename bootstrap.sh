8231a01d38f2f09c2bb2da311b027e6098b57440
Stream <g0110280@gmail.com>
2020-05-05 11:55:08 +0800

    create new script to install necessary tools

---
+++ bootstrap.sh
@@ -0,0 +1,2 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt grep
