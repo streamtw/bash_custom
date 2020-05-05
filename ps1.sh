aaef96a3a6498efd527a2bc667a66df81141e77e
Stream <g0110280@gmail.com>
2020-05-05 11:50:25 +0800

    fix ps1 gray color code

--- ps1.sh
+++ ps1.sh
@@ -2,8 +2,7 @@ function parse_git_branch {
     git branch 2> /dev/null | grep '*' | sed 's/*/ 💠/'
 }
 
-export GRAY='\[\033[1;90m\]'
-export GRAY_ALT='\[\033[0;30m\]'
+export GRAY='\[\033[0;1m\]'
 export RED='\[\033[0;31m\]'
 export GREEN='\[\033[0;32m\]'
 export YELLOW='\[\033[0;33m\]'
